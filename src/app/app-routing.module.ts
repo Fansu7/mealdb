6cc938b059af868c3c2036143e23a1bff66f594a
Alfonso Vidal <vidaltorres.alfonso@gmail.com>
2025-02-04 09:57:56 +0100

    enh: refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { MealHomeComponent } from './meals/meal-home/meal-home.component';
+import { MealsListComponent } from './meals/meals-list/meals-list.component';
+import { MealDetailComponent } from './meals/meal-detail/meal-detail.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MealHomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  { path: 'meals', component: MealsListComponent },
+  { path: 'meals/detail/:id', component: MealDetailComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './footer/footer.component';
+import { MealHomeComponent } from './meals/meal-home/meal-home.component';
+import { MealCardComponent } from './meals/meal-card/meal-card.component';
+import { MealsListComponent } from './meals/meals-list/meals-list.component';
+import { MealDetailComponent } from './meals/meal-detail/meal-detail.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent,
+    MealHomeComponent,
+    MealCardComponent,
+    MealsListComponent,
+    MealDetailComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [HttpClientModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
