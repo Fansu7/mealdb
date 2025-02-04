30852dc204013175b780f1b54252524a38630fcd
Alfonso Vidal <vidaltorres.alfonso@gmail.com>
2025-02-04 09:51:24 +0100

    feat: add meal service

---
+++ src/app/meals/meal.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IMeal, IMeals } from '../model/meal';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MealService {
+  constructor(private http: HttpClient) {}
+
+  getRandomMeal(): Observable<IMeals> {
+    const url = 'https://themealdb.com/api/json/v1/1/random.php';
+    return this.http.get<IMeals>(url);
+  }
+
+  getMeals(letter: string): Observable<IMeals> {
+    const url =
+      'https://www.themealdb.com/api/json/v1/1/search.php?f=' + letter;
+    return this.http.get<IMeals>(url);
+  }
+
+  getMealDetail(mealId: number): any {
+    const url =
+      'https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + mealId;
+    return this.http.get<IMeals>(url);
+  }
+}
